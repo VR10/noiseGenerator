ac58e10591240a5c6ad34945f400128e433762bc
Vincent Rambold <vincent@rambold.de>
2022-06-19 22:41:02 +0200

    Inital random noise generation script

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+from random import choice
+from random import randint
+from random import shuffle
+from datetime import date
 
+#the amount of noise generated in %
+noise_level=5
+if(noise_level>=50):
+    print("Script is only built for noise generation sub 50%")
+    exit()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+training_data = 'C:/Users/vince/Desktop/Studium/Thesis/FB15K-237.2/Release/train.txt'
+data = pd.read_csv(training_data, sep='\t',header=None)
+data.columns=["head","relation","tail"]
+rows=(len(data.index))
 
+#select random relations where nodes will be modified
+acceptor_triples=[]
+temp=list(range(rows))
+random.shuffle(temp)
+for x in range(int(rows*noise_level*0.01)):
+    acceptor_triples.append(temp.pop(0))
+print(acceptor_triples)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#select donor nodes from disjoint set of relations
+donor_triples=[]
+for x in range(int(rows*noise_level*0.01)):
+    donor_triples.append(temp.pop(0))
+print(donor_triples)
+printed_progress=''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#randomly select a head or tail node to be altered
+for x in range(len(donor_triples)):
+    head_or_tail=randint(0,1)
+    progress=int(x/len(donor_triples)*100)
+    if(progress!=printed_progress):
+        print(str(progress)+'%')
+        printed_progress=progress
+    if(head_or_tail==0):
+        data['head'][acceptor_triples[x]]=data['head'][donor_triples[x]]
+    else:
+        data['tail'][acceptor_triples[x]]=data['tail'][donor_triples[x]]
+
+print(data.head())
+outputfile='fb15k-237_train'+'_noise_'+str(noise_level)+'.txt'
+data.to_csv(outputfile,sep='\t',header=False,index=False)
